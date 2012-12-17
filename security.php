f8157424ffb2f4f7447329aadb77a04b49e72bb4
Raphael Diniz <raphael.diniz@dce.ufpb.br>
2012-12-17 17:28:05 -0300

    att no security.php

--- security.php
+++ security.php
@@ -3,19 +3,19 @@
 
 include 'mysql_connection.php';
 
-$_SG['abreSessao'] = true;         // Inicia a sess„o com um session_start()?
+$_SG['abreSessao'] = true;         // Inicia a sess√£o com um session_start()?
 
-$_SG['caseSensitive'] = false;     // Usar case-sensitive? Onde 'thiago' È diferente de 'THIAGO'
+$_SG['caseSensitive'] = false;     // Usar case-sensitive? Onde 'thiago' √© diferente de 'THIAGO'
 
-$_SG['validaSempre'] = true;       // Deseja validar o usu·rio e a senha a cada carregamento de p·gina?
-// Evita que, ao mudar os dados do usu·rio no banco de dado o mesmo contiue logado.
+$_SG['validaSempre'] = true;       // Deseja validar o usu√°rio e a senha a cada carregamento de p√°gina?
+// Evita que, ao mudar os dados do usu√°rio no banco de dado o mesmo contiue logado.
 
 
-$_SG['paginaLogin'] = 'login_de_usuario.html'; // P·gina de login
+$_SG['paginaLogin'] = 'login_de_‚Ä¨usuario.php'; // P√°gina de login
 
-$_SG['tabela'] = 'user';       // Nome da tabela onde os usu·rios s„o salvos
+$_SG['tabela'] = 'user';       // Nome da tabela onde os usu√°rios s√£o salvos
 
-// Verifica se precisa iniciar a sess„o
+// Verifica se precisa iniciar a sess√£o
 if ($_SG['abreSessao'] == true) {
     session_start();
 }
@@ -26,11 +26,11 @@ function validaUsuario($email, $senha) {
 
     $cS = ($_SG['caseSensitive']) ? 'BINARY' : '';
 
-    // Usa a funÁ„o addslashes para escapar as aspas
+    // Usa a fun√ß√£o addslashes para escapar as aspas
     $nemail = addslashes($email);
     $nsenha = addslashes($senha);
 
-    // Monta uma consulta SQL (query) para procurar um usu·rio
+    // Monta uma consulta SQL (query) para procurar um usu√°rio
     $sql = "SELECT * FROM user WHERE email='$email' and password='$senha';";
     $query = mysql_query($sql);
     $resultado = mysql_fetch_assoc($query);
@@ -38,41 +38,41 @@ function validaUsuario($email, $senha) {
     // Verifica se encontrou algum registro
     if (empty($resultado)) {
     	  echo "nada";
-        // Nenhum registro foi encontrado => o usu·rio È inv·lido
+        // Nenhum registro foi encontrado => o usu√°rio √© inv√°lido
         return false;
 
     } else {
-        // O registro foi encontrado => o usu·rio È valido
+        // O registro foi encontrado => o usu√°rio √© valido
 
-        // Definimos dois valores na sess„o com os dados do usu·rio
+        // Definimos dois valores na sess√£o com os dados do usu√°rio
         $_SESSION['userID'] = $resultado['id']; // Pega o valor da coluna 'id do registro encontrado no MySQL
         $_SESSION['userName'] = $resultado['name']; // Pega o valor da coluna 'nome' do registro encontrado no MySQL
         $_SESSION['userEmail'] = $resultado['email'];
 
-        // Verifica a opÁ„o se sempre validar o login
+        // Verifica a op√ß√£o se sempre validar o login
         if ($_SG['validaSempre'] == true) {
-            // Definimos dois valores na sess„o com os dados do login
-            $_SESSION['userLogin'] = $usuario;
-            $_SESSION['userPasswd'] = $senha;
+            // Definimos dois valores na sess√£o com os dados do login
+            $_SESSION['userEmail']  = $resultado['email'];
+            $_SESSION['userPasswd'] = $resultado['password'];
         }
 
         return true;
     }
 }
 
-//funÁ„o que direciona pra pagina de login caso n„o haja sess„o ativa
+//fun√ß√£o que direciona pra pagina de login caso n√£o haja sess√£o ativa
 function protegePagina() {
     global $_SG;
 
     if (!isset($_SESSION['userID']) OR !isset($_SESSION['userName']) OR !isset($_SESSION['userEmail'])) {
-        // N„o h· usu·rio logado, manda pra p·gina de login
+        // N√£o h√° usu√°rio logado, manda pra p√°gina de login
         endSession();
     } else if (isset($_SESSION['userID']) OR isset($_SESSION['userName'])) {
-        // H· usu·rio logado, verifica se precisa validar o login novamente
+        // H√° usu√°rio logado, verifica se precisa validar o login novamente
         if ($_SG['validaSempre'] == true) {
-            // Verifica se os dados salvos na sess„o batem com os dados do banco de dados
+            // Verifica se os dados salvos na sess√£o batem com os dados do banco de dados
             if (!validaUsuario($_SESSION['userEmail'], $_SESSION['userPasswd'])) {
-                // Os dados n„o batem, manda pra tela de login
+                // Os dados n√£o batem, manda pra tela de login
                 endSession();
             }
         }
@@ -80,12 +80,12 @@ function protegePagina() {
 }
 
 /**
- * FunÁ„o para expulsar um visitante
+ * Fun√ß√£o para expulsar um visitante
  */
 function endSession() {
     global $_SG;
 
-    // Remove as vari·veis da sess„o (caso elas existam)
+    // Remove as vari√°veis da sess√£o (caso elas existam)
     unset($_SESSION['userID'], $_SESSION['userName'], $_SESSION['userEmail'], $_SESSION['userLogin'], $_SESSION['userPasswd']);
 
     // Manda pra tela de login
