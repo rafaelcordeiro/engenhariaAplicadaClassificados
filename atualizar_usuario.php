918fbeeb5b2b622963be4f7ff90a0391ec304797
rafaelcordeiro <rafael.farias@dce.ufpb.br>
2013-02-14 14:06:53 -0800

    Create atualizar_usuario.php

---
+++ atualizar_usuario.php
@@ -0,0 +1,15 @@
+<?php
+
+    include 'mysql_connection.php';
+    require_once("security.php");
+  protegePagina();
+
+	$id =$_POST["id"];
+
+	$name     = $_POST["nome"];
+	$email    = $_POST["email"];
+	$password = $_POST["password"];
+
+	mysql_query("UPDATE user SET name ='$name', password='$password', email='$email'  WHERE id ='$id'");
+
+	mysql_close();
